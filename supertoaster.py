d48a25e2756e7966db4d67f98238913f101348ab
NotJae <108066595+NotJae@users.noreply.github.com>
2022-06-23 14:08:33 +0200

    Add files via upload

---
+++ supertoaster.py
@@ -0,0 +1,17 @@
+import time 
+from meinToaster import toaster
+
+class Supertoaster(toaster):
+
+    def __init__(self, schaechte, farbe):
+        super().__init__(schaechte ,farbe )
+        self.temperatur = 0
+
+    def toasten (self):
+        if self.anzahl_toasts == 0:
+            print("Es sind keine Toasts im Toaster")
+        elif self._temperatur > 500:
+            print ("der Toaster ist heissgleaufen")
+        else :
+            time.sleep(self.toast_zeit)
+            print("Die Toasts wurden getoastet")
